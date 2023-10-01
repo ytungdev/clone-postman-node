a26d3e770dde92e1d5aca2640982635e71761117
ytung <ytung.dev@gmail.com>
2023-10-02 00:22:06 +0100

    added : use axios to handle external request

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const express = require("express");
 var cors = require('cors')
-const JSONtoHTML=  require('./objToHTML.js');
+
+const axios = require('axios');
 
 const app = express();
 
-const corsOptions ={
-    origin:true, 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: true,
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
+
+// For post request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, () => {
     console.log("Application started and Listening on port 3000");
 });
@@ -22,27 +26,73 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.post("/req", (req, res) => {
-    //   res.send("Hello world!");
+
+app.post("/proxy", (req, res) => {
     const url = req.body.url;
     const method = req.body.method;
     const param = req.body.param;
-    console.log(req)
-    const result = {
-        'url': url,
-        'method': method,
-        'param': param
-    }
-    res.send(result);
-});
+    console.log(method, url, param);
 
+
+    let result = {
+        status:0,
+        req:{},
+        res:{},
+        err:{}
+    }
+
+    axios.interceptors.request.use(request => {
+        // console.log(request)
+        let {
+            transitional,
+            adapter,
+            transformRequest,
+            transformResponse,
+            data,
+            ...headers
+        } = request;
+        result.req = {
+            headers:headers,
+            data:data
+        }
+        return request
+    });
+
+    axios({
+        method: method,
+        url: url,
+        data: param,
+        params: param
+    }).then(function (response) {
+        // console.log(response.data)
+        result.res = {
+            data:response.data,
+            status:response.status,
+            statusText:response.statusText,
+            headers:response.headers
+        };
+        result.status = 1;
+        res.send(result);
+    }, (error) => {
+        result.err = error;
+        res.send(result);
+    });
+
+});
 
 app.get("/test", (req, res) => {
     //   res.send("Hello world!");
     // const queryString = window.location.search;
-    url = req.originalUrl
+    url = req.originalUrl;
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ url: url }));
 });
 
+app.post("/test", (req, res) => {
+    //   res.send("Hello world!");
+    // const queryString = window.location.search;
+    url = req.originalUrl;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ url: url }));
+});
 
